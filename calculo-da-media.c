82eb5bb883a6e3b968cc9a7c8e3892b02816b581
luis guilherme <tete42449@gmail.com>
2024-05-03 09:03:13 -0300

    Create calculo-da-media.c

---
+++ calculo-da-media.c
@@ -0,0 +1,21 @@
+// Faça um programa que receba quatronúmeros inteiros, calcule e mostre a soma desses números
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+float nota1 ,nota2 ,nota3 ,nota4 ,mediafinal;
+int main(int argc, char const *argv[])
+{
+    printf("informe a nota1:\n");
+scanf("%f" ,&nota1);
+    printf("informe a nota2:\n");
+scanf("%f" ,&nota2);
+    printf("informe a nota3:\n");
+scanf("%f" ,&nota3);
+    printf("informe a nota4:\n");
+scanf("%f" ,&nota4);
+mediafinal=(nota1+nota2+nota3+nota4)/4;
+printf("sua media final e:%.2f\n",mediafinal);
+}
